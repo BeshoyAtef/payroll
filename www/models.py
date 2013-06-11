7683689a3459817a276b4fb96d6f95056199bc14
Mina Nagy <someone@somewhere.com>
2013-06-11 14:08:28 +0200

    created the init models #1

--- www/models.py
+++ www/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import BaseUserManager , AbstractBaseUser
+from django.utils.timezone import utc
+import datetime
+from datetime import timedelta
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)     
+    email = models.EmailField(max_length=254, unique=True)
+    mobile = models.IntegerField(default=0)
+    ssn = models.IntegerField(default=0)        
+    salary = models.IntegerField(default=0)
+    
+    REQUIRED_FIELDS = ['name']  
+
+
+#Attendance: Date, Checkin time, Checkout time, Employee
+class Attendance(models.Model):
+    date = models.DateTimeField(default=datetime.datetime.now())
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    employee = models.ForeignKey(Employee)
+
+#Attendance Exception: Attendance ID, Checkin time, Checkout time
+class AttendanceException(models.Model):
+    attendance = models.ForeignKey(Attendance)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+#Company Downtime: Date, start time, end time
+class CompanyDowntime(models.Model):
+    date = models.DateTimeField(default=datetime.datetime.now())
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    REQUIRED_FIELDS = ['start_time','end_time']  
+
+
+#Item: ID, name ,value
+''' "identifier" has been used for the naming of the id , as django by default have an id for each model 
+and using and id again will be confusing  and a value has been added which represent the money the employee should get from this model'''
+class Item(models.Model):
+	identifier = models.CharField(max_length=100,unique=True)  
+	name = models.CharField(max_length=100)  
+	value = models.IntegerField(default=0)
+
+#Batches: ID, Employee ID, Date, item ID, Piece price, Size
+class Batches(models.Model):
+	employee = models.ForeignKey(Employee)
+	date = models.DateTimeField(default=datetime.datetime.now())
+	item = models.ForeignKey(Item)
+	item_price = models.IntegerField()
+	size = models.IntegerField(default=0)
+
+#Payment: ID, Date, Employee ID, amount
+class Payment(models.Model):
+	date = models.DateTimeField(default=datetime.datetime.now())
+	employee = models.ForeignKey(Employee)
+	amount = models.IntegerField()
+
+#Loans: ID, Date, Employee ID, Amount
+class Loan(models.Model):
+    date = models.DateTimeField(default=datetime.datetime.now())
+    employee = models.ForeignKey(Employee)
+    amount = models.IntegerField()
+
