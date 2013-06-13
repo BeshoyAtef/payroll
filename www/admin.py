437cbfe421f5faf85c59f2dbdcfd9c4b807c96e0
BeshoSmile <beshosmile@gmail.com>
2013-06-13 18:10:33 +0200

    edited the main admin index page #5

--- www/admin.py
+++ www/admin.py
@@ -1,34 +1,32 @@
 from django.contrib import admin
+from www.views import import_attendance
+from django.http import HttpResponse
+
 
 def my_view(request, *args, **kwargs):
-    return HttpResponse(" <a href='/admin'>here to do nothing</a>")
+    return HttpsResponse(" <a href='/admin'>here to do nothing</a>")
 
-# admin.site.register_view('somepath', my_view, 'My Fancy Admin View!')
 
 # And of course, this still works:
 from www.models import *
 
 admin.site.register(Employee)
-admin.site.register(AttendanceException)
-admin.site.register(CompanyDowntime)
+
 # admin.site.register(Item)
 admin.site.register(Batch)
-admin.site.register(Payment)
-admin.site.register(Loan)
 
-from django.contrib import admin
+
 from django.shortcuts import redirect
 
 from admin_views.admin import AdminViews
 
-
 class TestAdmin(AdminViews):
     admin_views = (
-                    ('Redirect to CNN test', 'redirect_to_cnn'),
-                    ('Test Method', 'http://www.revsys.com'),
+                    ('Test Report', 'redirect_to_cnn'),
+                    ('Upload Attendance-Log','import_attendance'),
         )
 
     def redirect_to_cnn(self, *args, **kwargs):
-        return redirect('http://www.cnn.com')
+        return HttpResponse(" <a href='/admin'>here to do nothing</a>")
 
 admin.site.register(Item, TestAdmin)
