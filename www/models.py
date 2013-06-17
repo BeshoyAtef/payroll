a55cb8e1f3cd8bfd3a3a6ff8f00571e6f2295568
Tharwat7 <tharwat.amr@gmail.com>
2013-06-18 00:58:02 +0200

    Work Under progress #6

--- www/models.py
+++ www/models.py
@@ -47,17 +47,114 @@ class Item(models.Model):
 
 #Batches: ID, Employee ID, Date, item ID, Piece price, Size
 class Batch(models.Model):
-	employee = models.ForeignKey(Employee)
-	date = models.DateTimeField(default=datetime.datetime.now())
-	item = models.ForeignKey(Item)
-	item_price = models.IntegerField()
-	size = models.IntegerField(default=0)
+    employee = models.ForeignKey(Employee)
+    date = models.DateTimeField(default=datetime.datetime.now())
+    item = models.ForeignKey(Item)
+    item_price = models.IntegerField()
+    size = models.IntegerField(default=0)
+
+def company_wide_output_yearly_report(desired_year):
+    total_batches = Batch.objects.filter(date__year = desired_year)
+    number_of_employees = len(Employee.objects.all())
+
+    Jan = Feb = March = April = May = June = July = August = September = October = November = December = 0
+
+    for batch in total_batches:
+        if batch.date.month == 1:
+            Jan = Jan + batch.size
+        if batch.date.month == 2:
+            Feb = Feb + batch.size
+        if batch.date.month == 3:
+            March = March + batch.size
+        if batch.date.month == 4:
+            April = April + batch.size
+        if batch.date.month == 5:
+            May = May + batch.size
+        if batch.date.month == 6:
+            June = June + batch.size
+        if batch.date.month == 7:
+            July = July + batch.size
+        if batch.date.month == 8:
+            August = August + batch.size
+        if batch.date.month == 9:
+            September = September + batch.size
+        if batch.date.month == 10:
+            October = October + batch.size
+        if batch.date.month == 11:
+            November = November + batch.size
+        if batch.date.month == 12:
+            December = December + batch.size
+
+    total_batches_size = Jan + Feb + March + April + May + June + July + August + September + October + November + December
+    batch_per_employee = total_batches_size/number_of_employees
+
+    Dict = {'Jan': Jan, 'Feb': Feb, 'March': March, 'April': April, 'May': May, 'June': June, 'July': July, 'August': August, 
+    ' September': September, 'October': October, 'November': November, 'December': December, 'batch_per_employee': batch_per_employee}
+
+    return Dict
+
+def company_wide_output_monthly_report(desired_year, desired_month):
+    total_batches = Batch.objects.filter(date__year = desired_year, date__month = desired_month)
+    number_of_employees = len(Employee.objects.all())
+    list_of_output_per_day = []
+    for batches in total_batches:
+        day = batches.date.day
+        print day
+        print list_of_output_per_day[day]
+        list_of_output_per_day[day] = list_of_output_per_day[day] + batches.size 
+
+
+    number_of_items_in_list = len(list_of_output_per_day)
+    list_of_output_per_day = range(number_of_items_in_list)
+    total = sum(list_of_output_per_day) 
+    batch_per_employee = total/number_of_employees
+
+    Dict = {'list_of_output_per_day': list_of_output_per_day, 'batch_per_employee': batch_per_employee}
+
+    return Dict
+
 
 #Payment: ID, Date, Employee ID, amount
 class Payment(models.Model):
-	date = models.DateTimeField(default=datetime.datetime.now())
-	employee = models.ForeignKey(Employee)
-	amount = models.IntegerField()
+    date = models.DateTimeField(default=datetime.datetime.now())
+    employee = models.ForeignKey(Employee)
+    amount = models.IntegerField()
+
+def company_wide_salary_report(desired_year):
+    yearly_payments = Payment.objects.filter(date__year = desired_year)
+    Jan = Feb = March = April = May = June = July = August = September = October = November = December = 0  
+
+    for payment in yearly_payments:
+        print 'x'
+        if payment.date.month == 1:
+            Jan = Jan + payment.amount
+        if payment.date.month == 2:
+            Feb = Feb + payment.amount
+        if payment.date.month == 3:
+            March = March + payment.amount
+        if payment.date.month == 4:
+            April = April + payment.amount
+        if payment.date.month == 5:
+            May = May + payment.amount
+        if payment.date.month == 6:
+            June = June + payment.amount
+        if payment.date.month == 7:
+            July = July + payment.amount
+        if payment.date.month == 8:
+            August = August + payment.amount
+        if payment.date.month == 9:
+            September = September + payment.amount
+        if payment.date.month == 10:
+            October = October + payment.amount
+        if payment.date.month == 11:
+            November = November + payment.amount
+        if payment.date.month == 12:
+            December = December + payment.amount
+
+    Dict = {'Jan': Jan, 'Feb': Feb, 'March': March, 'April': April, 'May': May, 'June': June, 'July': July, 'August': August, 
+    ' September': September, 'October': October, 'November': November, 'December': December}
+    
+    return Dict
 
 #Loans: ID, Date, Employee ID, Amount
 class Loan(models.Model):
--- www/views.py
+++ www/views.py
@@ -14,107 +14,25 @@ def view_reports(request):
 
 def company_wide_salary_report(request):
 	desired_year = request.GET['year']
-	yearly_payments = Payment.objects.filter(date__year = desired_year)
-	
-	Jan = Feb = March = April = May = June = July = August = September = October = November = December = 0  
-
-	for payment in yearly_payments:
-		if payment.date.Month = 1:
-			Jan = Jan + payment.amount
-		if payment.date.Month = 2:
-			Feb = Feb + payment.amount
-		if payment.date.Month = 3:
-			March = March + payment.amount
-		if payment.date.Month = 4:
-			April = April + payment.amount
-		if payment.date.Month = 5:
-			May = May + payment.amount
-		if payment.date.Month = 6:
-			June = June + payment.amount
-		if payment.date.Month = 7:
-			July = July + payment.amount
-		if payment.date.Month = 8:
-			August = August + payment.amount
-		if payment.date.Month = 9:
-			September = September + payment.amount
-		if payment.date.Month = 10:
-			October = October + payment.amount
-		if payment.date.Month = 11:
-			November = November + payment.amount
-		if payment.date.Month = 12:
-			December = December + payment.amount
-	
-	Dict = {'Jan': Jan, 'Feb': Feb, 'March': March, 'April': April, 'May': May, 'June': June, 'July': July, 'August': August, 
-	' September': September, 'October': October, 'November': November, 'December': December}
-
+	Dict = company_wide_salary_report(desired_year)
 	return render(request, Dict)
 
 def company_wide_output_yearly_report(request):
 	desired_year = request.GET['year']
-	total_batches = Batch.objects.filter(date__year = desired_year)
-	number_of_employees = len(Employee.objects.all())
-
-	Jan = Feb = March = April = May = June = July = August = September = October = November = December = 0
-
-	for batches in total_batches:
-		if batch.date.Month = 1:
-			Jan = Jan + batch.size
-		if batch.date.Month = 2:
-			Feb = Feb + batch.size
-		if batch.date.Month = 3:
-			March = March + batch.size
-		if batch.date.Month = 4:
-			April = April + batch.size
-		if batch.date.Month = 5:
-			May = May + batch.size
-		if batch.date.Month = 6:
-			June = June + batch.size
-		if batch.date.Month = 7:
-			July = July + batch.size
-		if batch.date.Month = 8:
-			August = August + batch.size
-		if batch.date.Month = 9:
-			September = September + batch.size
-		if batch.date.Month = 10:
-			October = October + batch.size
-		if batch.date.Month = 11:
-			November = November + batch.size
-		if batch.date.Month = 12:
-			December = December + batch.size
-
-	total_batches_size = Jan + Feb + March + April + May + June + July + August + September + October + November + December
-	batch_per_employee = total_batches_size/number_of_employees
-
-	Dict = {'Jan': Jan, 'Feb': Feb, 'March': March, 'April': April, 'May': May, 'June': June, 'July': July, 'August': August, 
-	' September': September, 'October': October, 'November': November, 'December': December, 'batch_per_employee': batch_per_employee}
-
+	Dict = company_wide_output_yearly_report(desired_year)
 	return render(request, Dict)
 
 def company_wide_output_monthly_report(request):
 	desired_year = request.GET['year']
 	desired_month = request.GET['month']
-	total_batches = Batch.objects.filter(date__year = desired_year, date__month = desired_month)
-	number_of_employees = len(Employee.objects.all())
-	list_of_output_per_day = []
-	for batches in total_batches:
-		day = batches.date.day
-		list_of_output_per_day[day] = list_of_output_per_day[day] + batches.size 
-
-
-	number_of_items_in_list = len(list_of_output_per_day)
-	list_of_output_per_day = range(number_of_items_in_list)
-	total = sum(list_of_output_per_day) 
-	batch_per_employee = total/number_of_employees
-
-	Dict = {'number_of_items_in_list': number_of_items_in_list, 'batch_per_employee': number_of_items_in_list}
-
+	Dict = company_wide_output_monthly_report(desired_year, desired_month)
 	return render(request, Dict)
 
-def company_wide_output_monthly_report(request):
-	desired_year = request.GET['year']
-	desired_month = request.GET['month']
-	total_batches = len(Batch.objects.filter(date = year w month))
-	total_items = len(Item.objects.filter(date = year w month))
+# def company_wide_output_monthly_report(request):
+# 	desired_year = request.GET['year']
+# 	desired_month = request.GET['month']
+# 	total_batches = len(Batch.objects.filter(date__year = desired_year, date__month =  desired_month))
+# 	total_items = len(Item.objects.filter(date__year = desired_year, date__month =  desired_month))
 
-	return render({'Total Batches': total_batches, 'Total Items': total_items})
+# 	return render({'Total Batches': total_batches, 'Total Items': total_items})
 
